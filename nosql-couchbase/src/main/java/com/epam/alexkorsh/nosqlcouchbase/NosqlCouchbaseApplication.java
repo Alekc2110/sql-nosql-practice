083cb8a6ee558d0bc4e78980cfdb25179efcc02a
Oleksandr Korshunovych <oleksandr_korshunovych@epam.com>
2023-04-19 19:11:01 +0300

    added configuration

---
+++ nosql-couchbase/src/main/java/com/epam/alexkorsh/nosqlcouchbase/NosqlCouchbaseApplication.java
@@ -0,0 +1,13 @@
+package com.epam.alexkorsh.nosqlcouchbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NosqlCouchbaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NosqlCouchbaseApplication.class, args);
+    }
+
+}
